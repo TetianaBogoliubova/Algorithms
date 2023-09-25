c35f766b65a793e544577d0e2abff404d963a7e3
Bogoliubova.Tetiana <bogol.tatyana@gmail.com>
2023-09-25 16:01:51 +0200

    HW_19.09.23

---
+++ src/main/java/org/example/homework_19_09_23/ArrayTask.java
@@ -0,0 +1,44 @@
+package org.example.homework_19_09_23;
+
+// 1 уровень сложности: Дан отсортированный массив, в котором все элементы встречаются дважды
+// (один за другим), а один элемент появляется только один раз.
+// Написать метод для нахождения этого элемента.
+// Решить задачу:
+// через простой цикл
+// 2*. через бинарный поиск
+public class ArrayTask {
+
+    public void searchLinerInArray(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if ((i == 0 || arr[i] != arr[i - 1]) && (i == arr.length - 1 || arr[i] != arr[i + 1])) {
+                System.out.println("Liner search: " + arr[i]);
+            }
+        }
+    }
+
+    public int searchBinaryInArray(int[] arr) {
+        int firstIndex = 0;
+        int endIndex = arr.length - 1;
+
+        while (firstIndex < endIndex) {
+            int middleIndex = firstIndex + (endIndex - firstIndex) / 2;
+
+            if (middleIndex % 2 == 1) {
+                if (arr[middleIndex] == arr[middleIndex - 1]) {
+                    firstIndex = middleIndex + 1;
+                } else {
+                    endIndex = middleIndex - 1;
+                }
+            } else {
+                if (arr[middleIndex] == arr[middleIndex + 1]) {
+                    firstIndex = middleIndex + 2;
+                } else {
+                    endIndex = middleIndex - 1;
+                }
+            }
+        }
+        return arr[firstIndex];
+    }
+}
+
---
+++ src/main/java/org/example/homework_19_09_23/Main.java
@@ -0,0 +1,12 @@
+package org.example.homework_19_09_23;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayTask arrayTask = new ArrayTask();
+
+        int[] array = {5, 5, 6, 7, 7, 8, 8, 9, 9};
+
+        arrayTask.searchLinerInArray(array);
+        System.out.println("Binary search: " + arrayTask.searchBinaryInArray(array));
+    }
+}
